6ca4ba47a010bae401055da085e00c97c8b6f5f6
Edmark Mendoza <darkvoids80@gmail.com>
2026-09-18 15:36:12 +0800

    Commit Message

---
+++ app/src/test/java/quarter2/LimMinipeta1.java
@@ -0,0 +1,19 @@
+package quarter2;
+import org.junit.Test;
+public class LimMinipeta1 {
+    @Test
+    public void printMyProfile() {
+        String myName = "Edmark E. Mendoza";
+        String petName = "Destroyah";
+        String favFood = "Kare-kare";
+        int myAge = 17;
+
+        System.out.println("--- My Digital Profile ---");
+        System.out.println("Hello!, my name is " + myName + " and i am " + myAge);
+        System.out.println("And I know a wonderful cat named " + petName + ".");
+        System.out.println("And one of my favorite food is " + favFood + ".");
+    }
+
+
+
+}
